ed170c39bdde3a58c3c57873007bb345e5979997
Sandrina <Alexandrinasava55@gmail.com>
2024-08-17 18:16:46 +0300

    second try

--- precode_test.go
+++ precode_test.go
@@ -3,51 +3,12 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 
@@ -59,8 +20,6 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	expectedResponse := strings.Join(cafeList["moscow"], ",")
-	assert.Equal(t, expectedResponse, responseRecorder.Body.String())
 	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), len(cafeList["moscow"]))
 	// здесь нужно добавить необходимые проверки
 }
@@ -84,5 +43,5 @@ func TestMainHandlerWhenCorrectRequest(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
+	assert.NotEmpty(t, responseRecorder.Body)
 }
